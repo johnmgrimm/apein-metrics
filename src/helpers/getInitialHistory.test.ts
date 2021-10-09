6eb1cbaaa6a6d8aa84b37b4a18ad743a2a93f78d
johnmgrimm <jmgtc@protonmail.com>
2021-10-09 20:03:25 +0200

    Fix tests

--- src/helpers/getInitialHistory.test.ts
+++ src/helpers/getInitialHistory.test.ts
@@ -1,30 +1,21 @@
 import { getInitialHistory } from './getInitialHistory';
+import MockDate from 'mockdate';
 
-let dateNowSpy: any;
-
-beforeAll(() => {
-  // Lock Time
-  dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => 1633125600000);
-});
-
-afterAll(() => {
-  // Unlock Time
-  dateNowSpy.mockRestore();
-});
+MockDate.set('2021-10-02');
 
 describe('getInitialHistory', () => {
   test('returns only required data', async () => {
     expect(getInitialHistory(3)).toStrictEqual([
       {
-        date: 1633478400000,
+        date: 1632960000000,
         value: 0,
       },
       {
-        date: 1633564800000,
+        date: 1633046400000,
         value: 0,
       },
       {
-        date: 1633651200000,
+        date: 1633132800000,
         value: 0,
       },
     ]);
